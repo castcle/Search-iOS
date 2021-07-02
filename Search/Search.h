69dea3a675c76d2f5c8b3e84018c219eaed3b204
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-07-02 18:23:45 +0700

    Init Search module

---
+++ Search/Search.h
@@ -0,0 +1,18 @@
+//
+//  Search.h
+//  Search
+//
+//  Created by Tanakorn Phoochaliaw on 2/7/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Search.
+FOUNDATION_EXPORT double SearchVersionNumber;
+
+//! Project version string for Search.
+FOUNDATION_EXPORT const unsigned char SearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Search/PublicHeader.h>
+
+
