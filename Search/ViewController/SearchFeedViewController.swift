5636bd80e190b77f460f90f3f7d871dc33af3aa0
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2021-12-07 19:49:40 +0700

    Support report content

--- Search/ViewController/SearchFeedViewController.swift
+++ Search/ViewController/SearchFeedViewController.swift
@@ -204,6 +204,19 @@ extension SearchFeedViewController: HeaderTableViewCellDelegate {
     func didAuthen(_ headerTableViewCell: HeaderTableViewCell) {
         Utility.currentViewController().presentPanModal(AuthenOpener.open(.signUpMethod) as! SignUpMethodViewController)
     }
+    
+    func didReportSuccess(_ headerTableViewCell: HeaderTableViewCell) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1 ) {
+            Utility.currentViewController().navigationController?.pushViewController(ComponentOpener.open(.reportSuccess(true, "")), animated: true)
+        }
+        
+        if let indexPath = self.tableView.indexPath(for: headerTableViewCell) {
+            UIView.transition(with: self.tableView, duration: 0.35, options: .transitionCrossDissolve, animations: {
+                self.viewModel.feeds.remove(at: indexPath.section)
+                self.tableView.reloadData()
+            })
+        }
+    }
 }
 
 extension SearchFeedViewController: FooterTableViewCellDelegate {
