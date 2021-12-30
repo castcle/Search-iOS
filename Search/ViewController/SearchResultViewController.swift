b340196a225a183783ba9b082223e15ceedb7c3f
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2021-12-30 11:22:25 +0700

    Hide people

--- Search/ViewController/SearchResultViewController.swift
+++ Search/ViewController/SearchResultViewController.swift
@@ -215,12 +215,12 @@ class SearchResultViewController: ButtonBarPagerTabStripViewController, UITextFi
         vc3?.pageTitle = "Photo"
         let child_3 = vc3 ?? SearchFeedViewController()
         
-        let vc4 = SearchOpener.open(.searchFeed(SearchFeedViewModel(stage: self.viewModel.searchFeedStage, feedRequest: self.viewModel.feedRequest))) as? SearchFeedViewController
-        vc4?.pageIndex = 3
-        vc4?.pageTitle = "People"
-        let child_4 = vc4 ?? SearchFeedViewController()
+//        let vc4 = SearchOpener.open(.searchFeed(SearchFeedViewModel(stage: self.viewModel.searchFeedStage, feedRequest: self.viewModel.feedRequest))) as? SearchFeedViewController
+//        vc4?.pageIndex = 3
+//        vc4?.pageTitle = "People"
+//        let child_4 = vc4 ?? SearchFeedViewController()
 
-        return [child_1, child_2, child_3, child_4]
+        return [child_1, child_2, child_3]
     }
     
     @IBAction func clearAction(_ sender: Any) {
