c01251ba0a3ad305cddf325d5c924d1a6d8f791c
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2022-08-10 00:21:32 +0700

    Update model realm

--- Search/ViewController/SearchFeedViewController.swift
+++ Search/ViewController/SearchFeedViewController.swift
@@ -321,7 +321,7 @@ extension SearchFeedViewController: HeaderTableViewCellDelegate {
 }
 
 extension SearchFeedViewController: FooterTableViewCellDelegate {
-    func didTabQuoteCast(_ footerTableViewCell: FooterTableViewCell, content: Content, page: Page) {
+    func didTabQuoteCast(_ footerTableViewCell: FooterTableViewCell, content: Content, page: PageRealm) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             let viewController = PostOpener.open(.post(PostViewModel(postType: .quoteCast, content: content, page: page)))
             viewController.modalPresentationStyle = .fullScreen
